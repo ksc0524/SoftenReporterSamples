526e1b10b07c195749c4baf416f1ec7568049989
ksc0524 <ksc0524@gmail.com>
2018-03-24 20:34:03 +0900

    Excel data input sample added. Common collections added for XSSF.

--- SoftenReporterSamples/samples/make-pptx-report.cmd
+++ SoftenReporterSamples/samples/make-pptx-report.cmd
@@ -3,3 +3,5 @@ java -cp .;lib/* com.softenware.report.SoftenReporterProcess -template template\
 java -cp .;lib/* com.softenware.report.SoftenReporterProcess -template template\Template-Sample-A.pptx -in data\InputData.kson -data kson -out result\Report-Result-fromKsonInput.pptx
 
 java -cp .;lib/* com.softenware.report.SoftenReporterProcess -template template\Template-Sample-A.pptx -in data\InputData.json -data json -out result\Report-Result-fromJsonInput.pptx
+
+java -cp .;lib/* com.softenware.report.SoftenReporterProcess -template template\Template-Sample-A.pptx -in data\InputData.xlsx -data xlsx -out result\Report-Result-fromXlsxInput.pptx -date yy,MM/dd
